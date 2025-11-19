431ca94f76fdac2ebe3c750a5a67d945382f15a6
Jason zhao <>
2025-11-18 21:07:15 -0500

    watchdog: do not start if launcher already running; avoid restart loop

--- ProjectXS/ProjectXSLauncher/watchdog_start_launcher.sh
+++ ProjectXS/ProjectXSLauncher/watchdog_start_launcher.sh
@@ -27,6 +27,13 @@ cleanup() {
 trap cleanup EXIT
 
 while true; do
+  # If an Electron launcher process is already running, don't try to start another
+  EXISTING_LAUNCHER_PID=$(pgrep -f "$WORKDIR/node_modules/electron/dist/Electron.app" | head -n 1 || true)
+  if [ -n "$EXISTING_LAUNCHER_PID" ]; then
+    echo "[$(date -u '+%Y-%m-%d %H:%M:%S')] Detected existing launcher PID $EXISTING_LAUNCHER_PID - not starting another instance. Exiting watchdog." >> "$LOGFILE"
+    break
+  fi
+
   echo "[$(date -u '+%Y-%m-%d %H:%M:%S')] Starting launcher..." >> "$LOGFILE"
   npm start >> "$LOGFILE" 2>&1
   EXIT_CODE=$?
